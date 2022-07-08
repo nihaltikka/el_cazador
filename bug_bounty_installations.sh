c3588321a449f971333d7a0d0c70d8c6befc894a
Nihal Tikka <32479798+nihaltikka@users.noreply.github.com>
2022-07-08 09:28:03 +0530

    Update and rename bug_bounty_installations.txt to bug_bounty_installations.sh

---
+++ bug_bounty_installations.sh
@@ -0,0 +1,31 @@
+git clone https://github.com/YashGoti/crtsh.git
+
+
+wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
+chmod +x findomain-linux
+./findomain-linux
+
+go get -u github.com/tomnomnom/assetfinder
+
+git clone https://github.com/aboul3la/Sublist3r.git
+
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+
+git clone https://github.com/six2dez/reconftw
+cd reconftw/
+./install.sh
+
+
+git clone https://github.com/nahamsec/bbht.git
+cd bbht
+chmod +x install.sh
+./install.sh
+
+git clone https://github.com/defparam/smuggler.git
+
+go get -u github.com/vsec7/xkeys
+
+apt-get install sqlmap
+
+go get -u -v github.com/lukasikic/subzy
