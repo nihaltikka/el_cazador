336a917d2683e99c1d2f9df21c53079db6c0c793
Nihal Tikka <32479798+nihaltikka@users.noreply.github.com>
2022-07-10 22:37:51 +0530

    Update Pre_Hunt_installations.sh

--- Pre_Hunt_installations.sh
+++ Pre_Hunt_installations.sh
@@ -10,15 +10,14 @@ mkdir Tools
 git clone https://github.com/YashGoti/crtsh.git
 wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
 chmod +x findomain-linux
-./findomain-linux
 go get -u github.com/tomnomnom/assetfinder
 go install github.com/tomnomnom/httprobe@latest
 git clone https://github.com/aboul3la/Sublist3r.git
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 git clone https://github.com/devanshbatham/ParamSpider
-cd ParamSpider
-pip3 install -r requirements.txt
-mv ParamSpider /root/Tools/
+pip3 install -r /root/ParamSpider/requirements.txt 
+mv /root/ParamSpider /root/Tools/
 git clone https://github.com/defparam/smuggler.git
+mv  /root/smuggler /root/Tools
 go get -u github.com/vsec7/xkeys
 go get -u -v github.com/lukasikic/subzy
