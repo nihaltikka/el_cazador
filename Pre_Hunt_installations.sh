0526db5d13e88d5369475961cefc2ed33bdde7fc
Nihal Tikka <32479798+nihaltikka@users.noreply.github.com>
2022-07-10 22:56:28 +0530

    Update Pre_Hunt_installations.sh

--- Pre_Hunt_installations.sh
+++ Pre_Hunt_installations.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade
-sudo apt install git
-sudo apt install golang
-sudo apt install sysvbanner
-sudo apt-get install sqlmap
+echo -e "\n Looking for System Update"
+sudo apt -y update
+echo -e "\n Looking for System Upgrade"
+sudo apt -y upgrade
+echo -e "\n Installing/Upgrading Git"
+sudo apt install  git
+echo -e "\n Installing/Upgrading Python"
+sudo apt-get install  python3
+echo -e "\n Installing/Upgrading golang"
+sudo apt install  golang
+echo -e "\n Installing/Upgrading banner"
+sudo apt install  sysvbanner
+echo -e "\n Installing/Upgrading wget"
+sudo apt-get install wget
+sudo apt install wget
+echo -e "\n Installing/Upgrading sqlmap"
+apt-get install sqlmap
 mkdir Projects
 mkdir Tools
+echo -e "\n Installing/Upgrading crtsh"
 git clone https://github.com/YashGoti/crtsh.git
+echo -e "\n Installing/Upgrading findomain"
 wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
 chmod +x findomain-linux
-go get -u github.com/tomnomnom/assetfinder
+echo -e "\n Installing/Upgrading anew"
+go install -v github.com/tomnomnom/anew@latest
+echo -e "\n Installing/Upgrading httprobe"
 go install github.com/tomnomnom/httprobe@latest
-git clone https://github.com/aboul3la/Sublist3r.git
+echo -e "\n Installing/Upgrading assetfinder"
+go install github.com/tomnomnom/assetfinder@latest
+echo -e "\n Installing/Upgrading waybackurls"
+go install github.com/tomnomnom/waybackurls@latest
+echo -e "\n Installing/Upgrading qsreplace"
+go install github.com/tomnomnom/qsreplace@latest
+echo -e "\n Installing/Upgrading subfinder"
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+echo -e "\n Installing/Upgrading httpx"
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+echo -e "\n Installing/Upgrading Nuclei"
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+echo -e "\n Installing/Upgrading nuclei-templates"
+git clone https://github.com/projectdiscovery/nuclei-templates.git
+nuclei update-templates
+echo -e "\n Installing/Upgrading naabu"
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+echo -e "\n Installing/Upgrading Sublist3r"
+git clone https://github.com/aboul3la/Sublist3r.git
+echo -e "\n Installing/Upgrading ParamSpider"
 git clone https://github.com/devanshbatham/ParamSpider
 pip3 install -r /root/ParamSpider/requirements.txt 
 mv /root/ParamSpider /root/Tools/
+echo -e "\n Installing/Upgrading smuggler
 git clone https://github.com/defparam/smuggler.git
-mv  /root/smuggler /root/Tools
+mv /root/smuggler /root/Tools
+echo -e "\n Installing/Upgrading XKeys"
 go get -u github.com/vsec7/xkeys
+echo -e "\n Installing/Upgrading subzy"
 go get -u -v github.com/lukasikic/subzy
