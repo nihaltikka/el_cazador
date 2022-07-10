e67aca4ebcf889f66cbc4fa02d68a5d666713635
Nihal Tikka <32479798+nihaltikka@users.noreply.github.com>
2022-07-10 22:09:40 +0530

    Update Pre_Hunt_installations.sh

--- Pre_Hunt_installations.sh
+++ Pre_Hunt_installations.sh
@@ -1,48 +1,23 @@
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt install -y git
-sudo apt install -y golang
-sudo apt install -y sysvbanner
-
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+sudo apt install golang
+sudo apt install sysvbanner
+sudo apt-get install sqlmap
+mkdir Projects
+mkdir Tools
 git clone https://github.com/YashGoti/crtsh.git
-
-
 wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
 chmod +x findomain-linux
 ./findomain-linux
-
 go get -u github.com/tomnomnom/assetfinder
-
 go install github.com/tomnomnom/httprobe@latest
-
 git clone https://github.com/aboul3la/Sublist3r.git
-
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-
-
-git clone https://github.com/six2dez/reconftw
-cd reconftw/
-chmod +x install.sh
-./install.sh
-
-
-git clone https://github.com/nahamsec/bbht.git
-cd bbht
-chmod +x install.sh
-./install.sh
-
 git clone https://github.com/devanshbatham/ParamSpider
 cd ParamSpider
 pip3 install -r requirements.txt
-
 mv ParamSpider /root/Tools/
-
 git clone https://github.com/defparam/smuggler.git
-
 go get -u github.com/vsec7/xkeys
-
-apt-get install sqlmap
-
 go get -u -v github.com/lukasikic/subzy
-
-mkdir Projects
