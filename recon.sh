3d47c40bff0e800ffa8c7582b4a076412f0a049d
Nihal Tikka <32479798+nihaltikka@users.noreply.github.com>
2023-02-06 16:23:12 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -60,7 +60,8 @@ nmap -sV -iL allsubdomains.txt -oN scanned-port.txt --script=vuln
 
 echo -e "\n Doing a complete directory Brute Force"
 
-python3 /root/Tools/dirsearch/dirsearch.py -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json -u https://+$domain+.com
+python3 /root/Tools/dirsearch/dirsearch.py -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json python3 /root/Tools/dirsearch/dirsearch.py -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json -l /root/Projects/$domain/sorted_subdomain.txt
+
 
 echo -e "\n Getting all URLS using waybackurls"
 
